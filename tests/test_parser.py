749fb69799f0870f78f4ed49937f9642e09f2225
Ben Burrill <bburrill98@gmail.com>
2025-09-26 22:54:48 -0700

    Add label tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -37,3 +37,66 @@ def test_format_conflict():
             %format output byte
             %format output unsigned
         """))
+
+def test_cyclic_labels():
+    assert make_program(get_lines("""
+        %format word 2
+        %section state
+        .word end - begin  ; 2 bytes
+        begin:
+        .ascii "Hello"     ; 5 bytes
+        .byte 0            ; 1 byte
+        .word 0            ; 2 bytes
+        .zero 2 * begin    ; begin = 2, so 4 bytes
+        .fill end, begin   ; begin = 2, so 2 bytes
+        end:
+    """)).state[0] == 5 + 1 + 2 + 4 + 2
+
+    with raises(LabelError, match='concrete'):
+        # Here the after label is defined, but has a cyclic dependency
+        # which cannot be resolved.
+        make_program(get_lines("""
+            %format word 2
+            %section state
+            .zero after
+            after:
+        """))
+
+    with raises(ExpressionError, match='defined'):
+        # The potato label is not defined at all.
+        make_program(get_lines("""
+            %format word 2
+            %section state
+            .zero potato
+            after:
+        """))
+
+def test_label_sections():
+    prog = make_program(get_lines("""
+        %format word 2
+        %section state
+        .word label_state
+        .zero 5
+        label_state:
+        
+        %section const
+        .word label_const
+        .zero 10
+        label_const:
+    """))
+    assert prog.state[0] == 2 + 5
+    assert prog.const[0] == 2 + 10
+
+    with raises(NameConflictError, match='redefined'):
+        make_program(get_lines("""
+            %format word 2
+            %section state
+            .word label
+            .zero 5
+            label:
+            
+            %section const
+            .word label
+            .zero 10
+            label:
+        """))
