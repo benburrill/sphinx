3292530c968c1d143ffcd95f7a814822c71ad780
Ben Burrill <bburrill98@gmail.com>
2025-10-17 17:01:31 -0700

    Refactor get_lines into parse

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -10,18 +10,17 @@ from spasm.parser import Parser as SphinxParser
 from spasm.context import output_map
 
 
-def get_lines(src, raw=False):
+def parse(src, *, args=(), raw=False):
     if not raw:
         src = textwrap.dedent(src).strip('\n')
-    return src.encode('utf-8').split(b'\n')
+    lines = src.encode('utf-8').split(b'\n')
 
-def parse(lines, *, args=()):
     ps = SphinxParser(args)
     ps.parse_lines(lines)
     return ps
 
-def make_program(lines, **kwargs):
-    return parse(lines, **kwargs).get_program()
+def make_program(src, **kwargs):
+    return parse(src, **kwargs).get_program()
 
 
 @pytest.mark.parametrize("src, word_size, output_format", [
@@ -36,7 +35,7 @@ def make_program(lines, **kwargs):
     ("%format word 3\n%format output byte", 3, 'byte')
 ])
 def test_formats(src, word_size, output_format):
-    ps = parse(get_lines(src))
+    ps = parse(src)
     prog = ps.get_program()
     ctx = ps.get_output_context()
     assert prog.mf.word_size == word_size
@@ -62,31 +61,31 @@ def test_formats(src, word_size, output_format):
 ])
 def test_format_errors(src, err_match):
     with raises(AssemblerSyntaxError, match=err_match):
-        make_program(get_lines(src))
+        make_program(src)
 
 
 def test_format_conflict():
     with raises(AssemblerError, match='conflict'):
-        make_program(get_lines("""
+        make_program("""
             %format word 2
             %format word 3
-        """))
+        """)
 
     with raises(AssemblerError, match='conflict'):
-        make_program(get_lines("""
+        make_program("""
             %format output byte
             %format output unsigned
-        """))
+        """)
 
 
 @pytest.mark.skipif(not math.isfinite(sys.maxsize), reason='Turing complete')
 def test_missing_integers_for_inf_word_size():
     with raises(OSError, match='Your system lacks some of the integers'):
-        make_program(get_lines("%format word inf"))
+        make_program("%format word inf")
 
 
 def test_cyclic_labels():
-    assert make_program(get_lines("""
+    assert make_program("""
         %format word 2
         %section state
         .word end - begin  ; 2 bytes
@@ -97,30 +96,30 @@ def test_cyclic_labels():
         .zero 2 * begin    ; begin = 2, so 4 bytes
         .fill end, begin   ; begin = 2, so 2 bytes
         end:
-    """)).state[0] == 5 + 1 + 2 + 4 + 2
+    """).state[0] == 5 + 1 + 2 + 4 + 2
 
     with raises(LabelError, match='concrete'):
         # Here the after label is defined, but has a cyclic dependency
         # which cannot be resolved.
-        make_program(get_lines("""
+        make_program("""
             %format word 2
             %section state
             .zero after
             after:
-        """))
+        """)
 
     with raises(ExpressionError, match='defined'):
         # The potato label is not defined at all.
-        make_program(get_lines("""
+        make_program("""
             %format word 2
             %section state
             .zero potato
             after:
-        """))
+        """)
 
 
 def test_label_sections():
-    prog = make_program(get_lines("""
+    prog = make_program("""
         %format word 2
         %section state
         .word label_state
@@ -131,12 +130,12 @@ def test_label_sections():
         .word label_const
         .zero 10
         label_const:
-    """))
+    """)
     assert prog.state[0] == 2 + 5
     assert prog.const[0] == 2 + 10
 
     with raises(NameConflictError, match='redefined'):
-        make_program(get_lines("""
+        make_program("""
             %format word 2
             %section state
             .word label
@@ -147,7 +146,7 @@ def test_label_sections():
             .word label
             .zero 10
             label:
-        """))
+        """)
 
 
 @pytest.mark.parametrize("expr, expected", [
@@ -164,11 +163,11 @@ def test_label_sections():
     ("('B' + 3) * 2", (ord('B') + 3) * 2)
 ])
 def test_math(expr, expected):
-    assert make_program(get_lines(f"""
+    assert make_program(f"""
         %format word 2
         %section state
         .word {expr}
-    """)).signed(('sv', 0)) == expected
+    """).signed(('sv', 0)) == expected
 
 
 @pytest.mark.parametrize("expr, error", [
@@ -181,30 +180,30 @@ def test_math(expr, expected):
 ])
 def test_bad_math(expr, error):
     with raises(error):
-        make_program(get_lines(f"""
+        make_program(f"""
             %format word 2
             %section state
             .word {expr}
-        """))
+        """)
 
 
 def test_word_suffix():
-    assert make_program(get_lines("""
+    assert make_program("""
         %format word 3
         %section state
         .word 10w
-    """)).signed(('sv', 0)) == 30
+    """).signed(('sv', 0)) == 30
 
-    assert make_program(get_lines("""
+    assert make_program("""
         %section state
         .word 10w
         %format word 3
-    """)).signed(('sv', 0)) == 30
+    """).signed(('sv', 0)) == 30
 
-    assert make_program(get_lines("""
+    assert make_program("""
         %section state
         .word 10w
-    """)).signed(('sv', 0)) == 20
+    """).signed(('sv', 0)) == 20
 
 
 @pytest.mark.parametrize("comment", ["", " ; comment"])
@@ -215,11 +214,11 @@ def test_word_suffix():
     (".word 1, 2, 3", b"\x01\x00\x02\x00\x03\x00")
 ])
 def test_directive_multi_expr(directive, expected, comment):
-    assert bytes(make_program(get_lines(f"""
+    assert bytes(make_program(f"""
         %format word 2
         %section state
         {directive}{comment}
-    """)).state) == expected
+    """).state) == expected
 
 
 @pytest.mark.parametrize("directive", [
@@ -232,11 +231,11 @@ def test_directive_multi_expr(directive, expected, comment):
 ])
 def test_bad_multi_expr(directive):
     with raises(AssemblerSyntaxError):
-        bytes(make_program(get_lines(f"""
+        bytes(make_program(f"""
             %format word 2
             %section state
             {directive}
-        """)).state)
+        """).state)
 
 
 @pytest.mark.parametrize("count", [0, 1, 100])
@@ -246,11 +245,11 @@ def test_bad_multi_expr(directive):
     (".fill 'B', {count}", b'B'),
 ])
 def test_fill(count, directive_fmt, fill_byte):
-    assert bytes(make_program(get_lines(f"""
+    assert bytes(make_program(f"""
         %format word 2
         %section state
         {directive_fmt.format(count=count)}
-    """)).state) == fill_byte * count
+    """).state) == fill_byte * count
 
 @pytest.mark.parametrize("directive, error, err_match", [
     (".zero -1", ExpressionError, 'must not be negative'),
@@ -266,8 +265,8 @@ def test_fill(count, directive_fmt, fill_byte):
 ])
 def test_bad_fill(directive, error, err_match):
     with raises(error, match=err_match):
-        bytes(make_program(get_lines(f"""
+        bytes(make_program(f"""
             %format word 2
             %section state
             {directive}
-        """)).state)
+        """).state)
